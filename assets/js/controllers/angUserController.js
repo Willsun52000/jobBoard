09594be52470548fa1b153b02dac3cbeab52bbc4
Willsun52000 <41751143@qq.com>
2016-11-26 17:06:10 +0800

    format js

--- assets/js/controllers/angUserController.js
+++ assets/js/controllers/angUserController.js
@@ -1,26 +1,29 @@
-define(function () {
-    return ['$scope', '$http', '$location', function($scope, $http, $location) {
-  
-    function resetItem(){
-       $scope.user = {
-          userid : '',
-          password : '',
-          repeatpassword : '',
-          error : ''
-       };    
-   $scope.displayForm = '';
+define(function() {
+  return ['$scope', '$http', '$location', function($scope, $http, $location) {
+
+    function resetItem() {
+      $scope.user = {
+        userid: '',
+        password: '',
+        repeatpassword: '',
+        error: ''
+      };
+      $scope.displayForm = '';
     }
     resetItem();
-     
-    $scope.signup = function () {
+
+    $scope.signup = function() {
       var user = $scope.user;
-      
+
       $http.get('/user/find?userid=' + user.userid).success(function(data) {
-        if(data!=null && data.length>0){
+        if (data != null && data.length > 0) {
           user.error = "用户名已存在";
-        }else{
-          $http.post('/user/create', user, {headers: {'X-CSRF-Token':$scope.csrf}}).success(function(data) {
-          }).
+        } else {
+          $http.post('/user/create', user, {
+            headers: {
+              'X-CSRF-Token': $scope.csrf
+            }
+          }).success(function(data) {}).
           error(function(data) {
             console.log(data);
           });
@@ -28,34 +31,34 @@ define(function () {
         }
       });
     };
-     
-    $scope.login = function () {
+
+    $scope.login = function() {
       var user = $scope.user;
-      
-      $http.get('/user/find?userid=' + user.userid ＋ ‘&password＝’ ＋ user.password).success(function(data) {
-        if(data==null){
+
+      $http.get('/user/find?userid=' + user.userid+'&password＝'+user.password).success(function(data) {
+        if (data == null) {
           user.error = "用户名密码错误";
-        }else{
+        } else {
           // $location.path('manage');
         }
       });
     };
-      
- $scope.addItem = function () {
-   resetItem();
-   $scope.displayForm = true;
- }
 
-      
-        $http.get('/csrfToken').success(function(data) {
-          $scope.csrf = data._csrf;
-        });
+    $scope.addItem = function() {
+      resetItem();
+      $scope.displayForm = true;
+    }
+
+
+    $http.get('/csrfToken').success(function(data) {
+      $scope.csrf = data._csrf;
+    });
     // $http.get('/user/find',user).success(function(data) {
     // });
- 
-        function removeModal(){
-          $('.modal').modal('hide');          
-      }
- 
-    }]; 
+
+    function removeModal() {
+      $('.modal').modal('hide');
+    }
+
+  }];
 });
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,30 @@
 require.config({
-urlArgs: "bust=" + (new Date()).getTime(),
-paths: {
-'angular': '../vendor/angular/angular.min',
-'jquery': '../vendor/jquery/jquery.min',
-'bootstrapJs': '../vendor/bootstrap/bootstrap'
-},
-/**
-* for libs that either do not support AMD out of the box, or
-* require some fine tuning to dependency mgt'
-*/
-shim: {
-'bootstrapJs': ['jquery'],
-'angular': {'exports': 'angular'}
-}
+	urlArgs: "bust=" + (new Date()).getTime(),
+	paths: {
+		'angular': '../vendor/angular/angular.min',
+		'jquery': '../vendor/jquery/jquery.min',
+		'bootstrapJs': '../vendor/bootstrap/bootstrap'
+	},
+	/**
+	 * for libs that either do not support AMD out of the box, or
+	 * require some fine tuning to dependency mgt'
+	 */
+	shim: {
+		'bootstrapJs': ['jquery'],
+		'angular': {
+			'exports': 'angular'
+		}
+	}
 });
 window.name = "NG_DEFER_BOOTSTRAP!";
 require([
-'angular',
-'app',
-'bootstrapJs'
+	'angular',
+	'app',
+	'bootstrapJs'
 ], function(angular, app) {
-'use strict';
-var $html = angular.element(document.getElementsByTagName('html')[0]);
-angular.element().ready(function() {
-angular.resumeBootstrap([app.name]);
+	'use strict';
+	var $html = angular.element(document.getElementsByTagName('html')[0]);
+	angular.element().ready(function() {
+		angular.resumeBootstrap([app.name]);
+	});
 });
-}
-);
